2702aca33658e07615a8921509158678544e6013
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-04-07 20:42:58 -0300

    sadf

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import "../styles/projects.css"
+import ProjectCard from "./ProjectCard.js"
 function Projects() {
   return (
-    <div className='projects' id="Projects">Projects</div>
+    <div className='projects' id="Projects">
+      <h1 className="latestProjects">
+        latest Projects
+      </h1>
+      <div className="cards">
+        <ProjectCard/>
+        <ProjectCard/>
+        <ProjectCard/>
+      </div>
+      <button className="more">Watch More projects</button>
+    </div>
   )
 }
 
