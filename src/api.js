0199d7478aceb5b645b1c1355f40053e159ee47b
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-04-09 21:21:12 -0300

    sdf

--- src/api.js
+++ src/api.js
@@ -10,33 +10,35 @@ import faq from "./images/faq.png"
 export const info=[
     {
         cardTitle:"Delta&Nature’s E-commerce webPage",
-        cardDescription:"a tourism start up´s web page,designed and developed by me using figma,html,css,javasScript and React. It has some unique animations using svg.",
+        cardDescription:"a tourism start up´s web page,I used figma,html,css,javasScript and React. It has some unique animations using svg.",
         cardImage:delta,
         photoRigth:true,
-        bg:"#711A75"
+        bg:"#711A75",
+        longDescription:""
     },
     {
         cardTitle:"Full stack CRUD comment Section",
-        cardDescription:"a comment section designed by Front end Mentor. I used html,css and React fort the front end and FireBase for user identification with google and database.",
+        cardDescription:"I used html,css and React fort the front end and FireBase for user identification with google and database.",
         cardImage:comments,
         photoRigth:false,
-        bg:"#F10086"
+        bg:"#F10086",
+        longDescription:""
     },
     {
         cardTitle:"Country sercher App",
         cardDescription:"a coutry data sercher app,designed by Front End Mentor. I used html,css, javaScript,React and consumed a Restful API",
         cardImage:countries,
         photoRigth:true,
-        bg:"#F582A7"
-
+        bg:"#F582A7",
+        longDescription:""
     },
     {
         cardTitle:"Animation project experience",
         cardDescription:'audio-visual experiense to present "raros peinados nuevos", an animation project,to the public and investors.coded in html,css,javascript and React',
         cardImage:raros,
         photoRigth:false,
-        bg:"#711A75"
-
+        bg:"#711A75",
+        longDescription:""
     },
     {
         cardTitle:"Special to do app",
@@ -51,31 +53,31 @@ export const info=[
         cardDescription:"designed by front end mentor,it displays information about de requested IP and show the location in an integrated map.I used html,css,React, an rest API for IP info and another API for the map",
         cardImage:tracker,
         photoRigth:false,
-        bg:"#F582A7"
-
+        bg:"#F582A7",
+        longDescription:""
     },
     {
         cardTitle:"Ecommerce product Main page",
         cardDescription:"designed by front end mentor, I used html,css,React and vanilla js for the drag and drop.",
         cardImage:ecommerce,
         photoRigth:true,
-        bg:"#711A75"
-
+        bg:"#711A75",
+        longDescription:""
     },
     {
         cardTitle:"Count Down Component",
         cardDescription:"designed by front end mentor, I used html,css and React. it turns with a cool animation ",
         cardImage:count,
         photoRigth:false,
-        bg:"#F10086"
-
+        bg:"#F10086",
+        longDescription:""
     },
     {
         cardTitle:"FAQ component",
         cardDescription:"designed by front end mentor, I used html,css and React.",
         cardImage:faq,
         photoRigth:true,
-        bg:"#F582A7"
-
+        bg:"#F582A7",
+        longDescription:""
     },
 ]
