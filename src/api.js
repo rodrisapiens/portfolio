a1a0efe5097f722def666907430d5cd42e397cd0
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-04-10 23:01:20 -0300

    about me

--- src/api.js
+++ src/api.js
@@ -99,3 +99,4 @@ export const info=[
         deployment:"https://faq-accordion-card-vert-xi.vercel.app/"
     },
 ]
+export const aboutMeText={title:"Hi! I’m Rodrigo Niveyro Reguero,a 23 year old web developer.",p1:"I studied Electronics Ingenieering since 2017 and that  gave me a solid background in math,softwareingeniering and scientific way of thinking.",p2:"Im passionate about creating cool stuff and believe in lerning as a life style",p3:"Lately I have been working as a freelance web developer and cryptomarket trader. I’m really looking forward to joining part of a team and keep learning new things",p4:"Im bilingual in english and spanish"}
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { ReactComponent as C } from "../images/c.svg"
+import { ReactComponent as Html } from "../images/html.svg"
+import { ReactComponent as Css } from "../images/cssIcon.svg"
+import { ReactComponent as Js } from "../images/js.svg"
+import { ReactComponent as ReactIcon } from "../images/reactIcon.svg"
+import { ReactComponent as Cplusplus } from "../images/cplusplus.svg"
+import { ReactComponent as Matlab } from "../images/matlab.svg"
 import "../styles/aboutme.css"
+import { aboutMeText } from "../api";
+import me from "../images/guy.png";
 function AboutMe() {
   return (
     <div className='AboutMe' id='AboutMe'>
-      AboutMe
+      <h1 className="aboutMeTitle">About Me</h1>
+      <img src={me} alt="" className="me" />
+      <div className="textConteiner">
+        <h1 className="aboutMeTextTitle">
+        {aboutMeText.title}
+        </h1>
+        <p className="aboutMeLittle">{aboutMeText.p1}</p>
+        <p className="aboutMeLittle">{aboutMeText.p2}</p>
+        <p className="aboutMeLittle">{aboutMeText.p3}</p>
+        <p className="aboutMeLittle">{aboutMeText.p4}</p>
+      </div>
+      <div className="icons">
+        <Html className='html icon' />
+        <Css className='css icon' />
+        <Js className='js icon' />
+        <ReactIcon className='reactIcon icon' />
+        <C className='c icon' />
+        <Cplusplus className='cplusplus icon' />
+        <Matlab className='matlab icon' />
+      </div>
     </div>
+
   )
 }
 
